7d83ab27902cd1f5fa191368711a680564fbe8cf
luochao <651644100@qq.com>
2020-12-01 18:28:12 +0800

    test: 完善了编辑器disable部分的测试

--- test/editor/disable.test.ts
+++ test/editor/disable.test.ts
@@ -18,15 +18,24 @@ describe('Editor disable', () => {
     })
 
     test('编辑器禁用后通过js修改内容，change hook监听会触发', done => {
-        const changeFn = jest.fn(() => {
-            done()
-        })
+        expect.assertions(1)
+
+        const changeFn = jest.fn()
 
         editor.disable()
 
         editor.txt.eventHooks.changeEvents.push(changeFn)
 
         editor.txt.html(`<span>123</span>`)
+
+        setTimeout(() => {
+            try {
+                expect(changeFn).toBeCalled()
+                done()
+            } catch (err) {
+                done.fail(err)
+            }
+        }, 500)
     })
 
     test('编辑器禁用后可以取消禁用', () => {
